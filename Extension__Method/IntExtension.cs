ab3fd29ea9bb9470dc6d0e14445118090d662e79
mohamedsherif301 <myosre122@gmail.com>
2025-06-03 11:56:38 +0300

    Extension Method Explan how to custom Method Help you more Vaildtion

---
+++ Extension__Method/IntExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension__Method
+{
+    public static class IntExtension
+    {
+        public static Boolean IsBetween(this int Value, int min , int max)
+        {
+            return Value >= min && Value <= max;
+
+        }
+    }
+}
---
+++ Extension__Method/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Extension__Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int presentage = 60;
+            if (presentage.IsBetween(0, 50))
+            {
+                Console.WriteLine("Valid Input ");
+            }
+            else
+            {
+                Console.WriteLine("InValid Input Number");
+
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
