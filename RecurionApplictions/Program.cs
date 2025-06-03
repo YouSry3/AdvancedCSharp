50921f7b333f49f48922dd00a71d1ed43dc6104b
mohamedsherif301 <myosre122@gmail.com>
2025-06-03 11:58:25 +0300

    Recurion Application and how to Loop & Determind Best Case Stop where !

---
+++ RecurionApplictions/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RecurionApplictions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 6;
+            Console.WriteLine(number.RecursionFactorial()); ;
+        }
+    }
+}
---
+++ RecurionApplictions/RecursionNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurionApplictions
+{
+    public static  class RecursionNumber
+    {
+        public static int RecursionFactorial(this int value ) {
+            if ( value <= 1 ) return 1;
+            return value * RecursionFactorial( value - 1 );
+        }
+    }
+}
